01f240a2e2db70d47f8fb8f74284077713386e55
DoSmile0705 <dev.lifecreator@gmail.com>
2024-02-26 15:40:33 -0800

    currency_converter

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Currency(Base):
+    __tablename__ = "currencies"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String)
+    code = Column(String, unique=True, index=True)
+    rate = Column(Float)
+    last_updated = Column(DateTime, default=datetime.utcnow)

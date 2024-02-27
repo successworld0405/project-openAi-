742fc5f1f5da55bff87ba7a65e2d533fd4f02558
DoSmile0705 <dev.lifecreator@gmail.com>
2024-02-26 19:26:21 -0800

    test

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["bash", "-c", "python -m alembic upgrade head && pytest && uvicorn app.main:app --host 0.0.0.0 --port 8000"]
--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:tjdrhd@localhost/postgres"
+DATABASE_URL = "postgresql://postgres:tjdrhd@db/postgres"
 
 
 engine = create_engine(DATABASE_URL)
